3ba90f5fe49f0cf4c59dceeff0e4c68aaaac2d2f
Adam Jackson <ajax@redhat.com>
2014-05-06 12:16:19 -0400

    xfree86: Convert xf86{Read,Write}Mmio{8,16,32} on alpha to functions
    ... instead of function pointers.  Since the sparse mapping support is
    gone these never vary anymore.
    
    Signed-off-by: Adam Jackson <ajax@redhat.com>

--- hw/xfree86/os-support/bsd/bsd_ev56.c
+++ hw/xfree86/os-support/bsd/bsd_ev56.c
@@ -21,53 +21,43 @@
  */
 __asm(".arch ev56");
 
-int readDense8(void *Base, register unsigned long Offset);
-int readDense16(void *Base, register unsigned long Offset);
-int readDense32(void *Base, register unsigned long Offset);
-void
- writeDense8(int Value, void *Base, register unsigned long Offset);
-void
- writeDense16(int Value, void *Base, register unsigned long Offset);
-void
- writeDense32(int Value, void *Base, register unsigned long Offset);
-
 int
-readDense8(void *Base, register unsigned long Offset)
+xf86ReadMmio8(void *Base, register unsigned long Offset)
 {
     mem_barrier();
     return (alpha_ldbu((void *) ((unsigned long) Base + (Offset))));
 }
 
 int
-readDense16(void *Base, register unsigned long Offset)
+xf86ReadMmio16(void *Base, register unsigned long Offset)
 {
     mem_barrier();
     return (alpha_ldwu((void *) ((unsigned long) Base + (Offset))));
 }
 
 int
-readDense32(void *Base, register unsigned long Offset)
+xf86ReadMmio32(void *Base, register unsigned long Offset)
 {
     mem_barrier();
     return *(volatile CARD32 *) ((unsigned long) Base + (Offset));
 }
 
 void
-writeDense8(int Value, void *Base, register unsigned long Offset)
+xf86WriteMmio8(int Value, void *Base, register unsigned long Offset)
 {
     write_mem_barrier();
     alpha_stb((void *) ((unsigned long) Base + (Offset)), Value);
 }
 
 void
-writeDense16(int Value, void *Base, register unsigned long Offset)
+xf86WriteMmio16(int Value, void *Base, register unsigned long Offset)
 {
     write_mem_barrier();
     alpha_stw((void *) ((unsigned long) Base + (Offset)), Value);
 }
 
 void
-writeDense32(int Value, void *Base, register unsigned long Offset)
+xf86WriteMmio32(int Value, void *Base, register unsigned long Offset)
 {
     write_mem_barrier();
     *(volatile CARD32 *) ((unsigned long) Base + (Offset)) = Value;
--- hw/xfree86/os-support/linux/lnx_ev56.c
+++ hw/xfree86/os-support/linux/lnx_ev56.c
@@ -6,53 +6,43 @@
 #include "xf86.h"
 #include "compiler.h"
 
-int readDense8(void *Base, register unsigned long Offset);
-int readDense16(void *Base, register unsigned long Offset);
-int readDense32(void *Base, register unsigned long Offset);
-void
- writeDense8(int Value, void *Base, register unsigned long Offset);
-void
- writeDense16(int Value, void *Base, register unsigned long Offset);
-void
- writeDense32(int Value, void *Base, register unsigned long Offset);
-
 int
-readDense8(void *Base, register unsigned long Offset)
+xf86ReadMmio8(void *Base, register unsigned long Offset)
 {
     mem_barrier();
     return *(volatile CARD8 *) ((unsigned long) Base + (Offset));
 }
 
 int
-readDense16(void *Base, register unsigned long Offset)
+xf86ReadMmio16(void *Base, register unsigned long Offset)
 {
     mem_barrier();
     return *(volatile CARD16 *) ((unsigned long) Base + (Offset));
 }
 
 int
-readDense32(void *Base, register unsigned long Offset)
+xf86ReadMmio32(void *Base, register unsigned long Offset)
 {
     mem_barrier();
     return *(volatile CARD32 *) ((unsigned long) Base + (Offset));
 }
 
 void
-writeDense8(int Value, void *Base, register unsigned long Offset)
+xf86WriteMmio8(int Value, void *Base, register unsigned long Offset)
 {
     write_mem_barrier();
     *(volatile CARD8 *) ((unsigned long) Base + (Offset)) = Value;
 }
 
 void
-writeDense16(int Value, void *Base, register unsigned long Offset)
+xf86WriteMmio16(int Value, void *Base, register unsigned long Offset)
 {
     write_mem_barrier();
     *(volatile CARD16 *) ((unsigned long) Base + (Offset)) = Value;
 }
 
 void
-writeDense32(int Value, void *Base, register unsigned long Offset)
+xf86WriteMmio32(int Value, void *Base, register unsigned long Offset)
 {
     write_mem_barrier();
     *(volatile CARD32 *) ((unsigned long) Base + (Offset)) = Value;
